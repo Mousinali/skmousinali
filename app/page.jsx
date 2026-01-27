28100110d292e6207201a9b2b57a8c695b45a906
Mousin <work.mousin@gmail.com>
2026-01-27 15:46:12 +0530

    Minor fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,15 @@
-import Image from "next/image";
 import Hero from "./components/Hero";
-import Service from "./components/section/Service";
-
 import About from "./components/section/About";
+import Service from "./components/section/Service";
 import PortfolioSection from "./components/section/portfolio";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Hero />
       <About />
       <Service />
       <PortfolioSection />
-    </main>
+    </>
   );
 }
